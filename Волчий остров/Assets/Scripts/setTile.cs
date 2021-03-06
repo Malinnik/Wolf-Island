bf49d39ddcabf2ad42aa7cead1e29b522ac6bfde
Malinnik <ratanov.maxim@gmail.com>
2021-03-06 21:12:24 +0300

    Wolfs update
    Добавлено движение и охота для волков
    Требуется исправить проблему со смертью и рождение кроликов

--- Волчий остров/Assets/Scripts/setTile.cs
+++ Волчий остров/Assets/Scripts/setTile.cs
@@ -5,22 +5,23 @@ using UnityEngine.Tilemaps;
 
 public class setTile : MonoBehaviour
 {
-    const int num = 399;  //Константная переменная хранящяя значение максимума существ
+    const int num = 400;  //Константная переменная хранящяя значение максимума существ
     public Tile groundTile;         //Тайл острова 
     public Tile rabbitTile;             //Тайл Кролика
     public Tile wolfTile;               //Тайл волка
     public Tilemap tilemap;         //Карта тайлов (из неё берётся тайлы для игры)
-    int[] rabbitX = new int[num+1];   //Массив X координат всех кроликов (0;num)
-    int[] rabbitY = new int[num+1];   //Массив Y координат всех кроликов (0;num)
-    int[] wolfX = new int[num + 1];   //Массив X координат всех волков (0;num)
-    int[] wolfY = new int[num + 1];   //Массив Y координат всех волков (0;num)
-    int[] wolf = new int[num + 1];    //Массив типов волков по полу (0-М, 1-Ж) (0;num)
+    bool[] isRabbitAlive = new bool[num]; //Жив ли кролик (0;num)
+    int[] rabbitX = new int[num];   //Массив X координат всех кроликов (0;num)
+    int[] rabbitY = new int[num];   //Массив Y координат всех кроликов (0;num)
+    int[] wolfX = new int[num];   //Массив X координат всех волков (0;num)
+    int[] wolfY = new int[num];   //Массив Y координат всех волков (0;num)
+    int[] wolf = new int[num];    //Массив типов волков по полу (0-М, 1-Ж) (0;num)
     int numOfWolfs = 1;             //Кол-во волков (0;num) //в начале игры должно быть минимум 2 волка, один М, другой Ж
     int numOfRabbits = 0;           //Кол-во кроликов (0;num)
+    Vector3Int cellPos;             //Позиция текущей клетки (курсора установки или удаления тайлов)
     [SerializeField] float timeScale;
     [SerializeField] int chanceOfDuplication = 20;   //Шанс размножения кроликов (0-100%)
-    
-    Vector3Int cellPos;             //Позиция текущей клетки (курсора установки или удаления тайлов)
+     
 
 
 
@@ -28,6 +29,7 @@ public class setTile : MonoBehaviour
     {   
         rabbitX[0] = 10;        //Создания начального кролика в координате 10 на 10
         rabbitY[0] = 10;
+        isRabbitAlive[0] = true;
         wolfX[0] = 1;
         wolfY[0] = 1;
         wolfX[1] = 20;
@@ -47,98 +49,119 @@ public class setTile : MonoBehaviour
 
         for (int i = 0; i <= numOfRabbits; i++) //Цикл передвижения всех кроликов 
         {
-            x = rabbitX[i]; //начальные координаты [i] кролика
-            y = rabbitY[i]; //начальные координаты [i] кролика     // 1 2 3
-                                                                   // 4 5 6
-            move = Random.Range(1, 9);  //путь движения кролика    // 7 8 9
-            switch (move)
+            if (isRabbitAlive[i] == false) { }
+            else
             {
-                case 1:
-                    rabbitX[i] = rabbitX[i] - 1;
-                    rabbitY[i] = rabbitY[i] + 1;
-                    break;
-                case 2:
-                    rabbitX[i] = rabbitX[i];
-                    rabbitY[i] = rabbitY[i] + 1;
-                    break;
-                case 3:
-                    rabbitX[i] = rabbitX[i] + 1;
-                    rabbitY[i] = rabbitY[i] + 1;
-                    break;
-                case 4:
-                    rabbitX[i] = rabbitX[i] - 1;
-                    rabbitY[i] = rabbitY[i];
-                    break;
-                case 5:
-                    rabbitX[i] = rabbitX[i];
-                    rabbitY[i] = rabbitY[i];
-                    break;
-                case 6:
-                    rabbitX[i] = rabbitX[i] + 1;
-                    rabbitY[i] = rabbitY[i];
-                    break;
-                case 7:
-                    rabbitX[i] = rabbitX[i] - 1;
-                    rabbitY[i] = rabbitY[i] - 1;
-                    break;
-                case 8:
-                    rabbitX[i] = rabbitX[i];
-                    rabbitY[i] = rabbitY[i] - 1;
-                    break;
-                case 9:
-                    rabbitX[i] = rabbitX[i] + 1;
-                    rabbitY[i] = rabbitY[i] - 1;
-                    break;
+                x = rabbitX[i]; //начальные координаты [i] кролика
+                y = rabbitY[i]; //начальные координаты [i] кролика      // 1 2 3
+                                                                        // 4 5 6
+                move = Random.Range(1, 10);  //путь движения кролика    // 7 8 9
+                switch (move)
+                {
+                    case 1:
+                        rabbitX[i] = rabbitX[i] - 1;
+                        rabbitY[i] = rabbitY[i] + 1;
+                        break;
+                    case 2:
+                        rabbitX[i] = rabbitX[i];
+                        rabbitY[i] = rabbitY[i] + 1;
+                        break;
+                    case 3:
+                        rabbitX[i] = rabbitX[i] + 1;
+                        rabbitY[i] = rabbitY[i] + 1;
+                        break;
+                    case 4:
+                        rabbitX[i] = rabbitX[i] - 1;
+                        rabbitY[i] = rabbitY[i];
+                        break;
+                    case 5:
+                        rabbitX[i] = rabbitX[i];
+                        rabbitY[i] = rabbitY[i];
+                        break;
+                    case 6:
+                        rabbitX[i] = rabbitX[i] + 1;
+                        rabbitY[i] = rabbitY[i];
+                        break;
+                    case 7:
+                        rabbitX[i] = rabbitX[i] - 1;
+                        rabbitY[i] = rabbitY[i] - 1;
+                        break;
+                    case 8:
+                        rabbitX[i] = rabbitX[i];
+                        rabbitY[i] = rabbitY[i] - 1;
+                        break;
+                    case 9:
+                        rabbitX[i] = rabbitX[i] + 1;
+                        rabbitY[i] = rabbitY[i] - 1;
+                        break;
+                }
+
+                //ограничения передвижения кролика (квадрат 20 на 20)
+                if (rabbitX[i] == 0)  rabbitX[i] = 1;  //Если вышел за левую грань   
+                {
+                    
+                }
+                if (rabbitX[i] == 21) rabbitX[i] = 20;  //Если вышел за правую грань
+                {
+                    
+                }
+                if (rabbitY[i] == 0)  rabbitY[i] = 1;  //Если вышел за нижнюю грань
+                {
+                    
+                }
+                if (rabbitY[i] == 21) rabbitY[i] = 20;  //Если вышел за верхнюю грань
+                {
+                    
+                }
+                //                      Y ^
+                //Система координат XY    | - >
+                //                            X
+
+                cellPos.x = x;
+                cellPos.y = y;
+                tilemap.SetTile(cellPos, groundTile); //Замена текущего тайла кролика на землю
+                cellPos.x = rabbitX[i];
+                cellPos.y = rabbitY[i];
+                tilemap.SetTile(cellPos, rabbitTile);  //Замена тайла земли на новую позицию кролика
+
+
+
+                //Размножение кроликов
+
+                plot = Random.Range(1, 100);  //Шанс размножения кролика = chaceOfDuplication%
+                if (plot <= chanceOfDuplication)
+                {
+
+                    for (int j = 0; j <= numOfRabbits; j++)
+                    {
+                        if (isRabbitAlive[j] == false)
+                        {
+                            isRabbitAlive[j] = true;
+                            rabbitX[j] = rabbitX[i];
+                            rabbitY[j] = rabbitY[i];
+                            cellPos.x = rabbitX[j];
+                            cellPos.y = rabbitX[j];
+                            tilemap.SetTile(cellPos, rabbitTile);
+                            break;
+                        }
+                        else if (numOfRabbits == num)
+                        {
+                            numOfRabbits -= 1; //Установка лимита кроликов (кол-во кроликов не будет превышать 399)
+                        
+                        } else 
+                        { 
+                            numOfRabbits += 1;  //Увеличение количества кроликов на 1
+                            rabbitX[numOfRabbits-1] = rabbitX[i];  //Координаты нового кролика = координатам родителя
+                            rabbitY[numOfRabbits-1] = rabbitY[i];
+                            cellPos.x = rabbitX[numOfRabbits-1];   //Изменение тайла на тайл кролика 
+                            cellPos.y = rabbitY[numOfRabbits-1];
+                            isRabbitAlive[numOfRabbits-1] = true;
+                            tilemap.SetTile(cellPos, rabbitTile);
+                            break;
+                        }
+                    }
+                }  
             }
-
-            //ограничения передвижения кролика (квадрат 20 на 20)
-            if (rabbitX[i] == 0)     //Если вышел за левую грань   
-            {
-                rabbitX[i] = 1;   
-            }
-            if (rabbitX[i] == 21)   //Если вышел за правую грань
-            {
-                rabbitX[i] = 20;
-            }
-            if (rabbitY[i] == 0)    //Если вышел за нижнюю грань
-            {
-                rabbitY[i] = 1;
-            }
-            if (rabbitY[i] == 21)  //Если вышел за верхнюю грань
-            {
-                rabbitY[i] = 20;
-            }
-            //                      Y ^
-            //Система координат XY    | - >
-            //                            X
-
-            cellPos.x = x; 
-            cellPos.y = y;
-            tilemap.SetTile(cellPos, groundTile); //Замена текущего тайла кролика на землю
-            cellPos.x = rabbitX[i];
-            cellPos.y = rabbitY[i];
-            tilemap.SetTile(cellPos, rabbitTile);  //Замена тайла земли на новую позицию кролика
-
-
-            
-            //Размножение кроликов
-
-            plot = Random.Range(1, 100);  //Шанс размножения кролика = 20%
-            if (plot <= chanceOfDuplication)
-            {
-                if (numOfRabbits == num) numOfRabbits -= 1; //Установка лимита кроликов (кол-во кроликов не будет превышать 399)
-                numOfRabbits += 1;  //Увеличение количества кроликов на 1
-                rabbitX[numOfRabbits] = rabbitX[i];  //Координаты нового кролика = координатам родителя
-                rabbitY[numOfRabbits] = rabbitY[i];
-                cellPos.x = rabbitX[numOfRabbits];   //Изменение тайла на тайл кролика 
-                cellPos.y = rabbitY[numOfRabbits];  
-                tilemap.SetTile(cellPos, rabbitTile);
-            }
-
-
-            
-
-
         }
     }
 
@@ -148,8 +171,8 @@ public class setTile : MonoBehaviour
          numOfRabbits = 0;
          for(int i = 1; i <= num; i++)
          {
-            rabbitX[i] = 0;
-            rabbitY[i] = 0;
+            rabbitX[i] = -1;
+            rabbitY[i] = -1;
          }
     }
 
@@ -183,6 +206,8 @@ public class setTile : MonoBehaviour
     void wolfMove()
     {
         int x, y;
+        bool[] isRabbitHere = new bool[9];
+        int[] rabbitIndex = new int[400];
         int move;
 
         for(int i = 0; i <= numOfWolfs; i++)
@@ -191,64 +216,125 @@ public class setTile : MonoBehaviour
             y = wolfY[i];
 
 
-            move = Random.Range(1, 9);  //путь движения волка
-            switch (move)
+            move = Random.Range(1, 10);  //путь движения волка
+
+            for (int j = 0; j <= numOfRabbits; j++)
             {
-                case 1:
-                    wolfX[i] = wolfX[i] - 1;
-                    wolfY[i] = wolfY[i] + 1;
-                    break;
-                case 2:
-                    wolfX[i] = wolfX[i];
-                    wolfY[i] = wolfY[i] + 1;
-                    break;
-                case 3:
-                    wolfX[i] = wolfX[i] + 1;
-                    wolfY[i] = wolfY[i] + 1;
-                    break;
-                case 4:
-                    wolfX[i] = wolfX[i] - 1;
-                    wolfY[i] = wolfY[i];
-                    break;
-                case 5:
-                    wolfX[i] = wolfX[i];
-                    wolfY[i] = wolfY[i];
-                    break;
-                case 6:
-                    wolfX[i] = wolfX[i] + 1;
-                    wolfY[i] = wolfY[i];
-                    break;
-                case 7:
-                    wolfX[i] = wolfX[i] - 1;
-                    wolfY[i] = wolfY[i] - 1;
-                    break;
-                case 8:
-                    wolfX[i] = wolfX[i];
-                    wolfY[i] = wolfY[i] - 1;
-                    break;
-                case 9:
-                    wolfX[i] = wolfX[i] + 1;
-                    wolfY[i] = wolfY[i] - 1;
-                    break;
+                if ((rabbitX[j] == wolfX[i] - 1) && (rabbitY[j] == wolfY[i] + 1)) isRabbitHere[0] = true;
+                if ((rabbitX[j] == wolfX[i])     && (rabbitY[j] == wolfY[i] + 1)) isRabbitHere[1] = true;
+                if ((rabbitX[j] == wolfX[i] + 1) && (rabbitY[j] == wolfY[i] + 1)) isRabbitHere[2] = true;
+
+                if ((rabbitX[j] == wolfX[i] - 1) && (rabbitY[j] == wolfY[i])) isRabbitHere[3] = true;
+                if ((rabbitX[j] == wolfX[i])     && (rabbitY[j] == wolfY[i])) isRabbitHere[4] = true;
+                if ((rabbitX[j] == wolfX[i] + 1) && (rabbitY[j] == wolfY[i])) isRabbitHere[5] = true;
+
+                if ((rabbitX[j] == wolfX[i] - 1) && (rabbitY[j] == wolfY[i] - 1)) isRabbitHere[6] = true;
+                if ((rabbitX[j] == wolfX[i])     && (rabbitY[j] == wolfY[i] - 1)) isRabbitHere[7] = true;
+                if ((rabbitX[j] == wolfX[i] + 1) && (rabbitY[j] == wolfY[i] - 1)) isRabbitHere[8] = true;
             }
 
+            if (isRabbitHere[5])
+            {
+                move = 5;
+                for (int j = 0; j <= numOfRabbits; j++)
+                {
+                    if ((rabbitX[j] == wolfX[i]) && (rabbitY[j] == wolfY[i]))
+                    {
+                        cellPos.x = rabbitX[j];
+                        cellPos.y = rabbitY[j];
+                        tilemap.SetTile(cellPos, groundTile);
+                        rabbitX[j] = -1;
+                        rabbitY[j] = -1;
+                        isRabbitAlive[j] = false;
+                    }
+                }
+
+            }
+
+
+
+            else if (isRabbitHere[1]) move = 1;
+            else if (isRabbitHere[2]) move = 2;
+            else if (isRabbitHere[2]) move = 3;
+            else if (isRabbitHere[2]) move = 4;
+            else if (isRabbitHere[2]) move = 6;
+            else if (isRabbitHere[2]) move = 7;
+            else if (isRabbitHere[2]) move = 8;
+            else if (isRabbitHere[2]) move = 9;
+
+
+            switch (move)
+                {
+                    case 1:
+                        wolfX[i] = wolfX[i] - 1;
+                        wolfY[i] = wolfY[i] + 1;
+                        break;
+                    case 2:
+                        wolfX[i] = wolfX[i];
+                        wolfY[i] = wolfY[i] + 1;
+                        break;
+                    case 3:
+                        wolfX[i] = wolfX[i] + 1;
+                        wolfY[i] = wolfY[i] + 1;
+                        break;
+                    case 4:
+                        wolfX[i] = wolfX[i] - 1;
+                        wolfY[i] = wolfY[i];
+                        break;
+                    case 5:
+                        wolfX[i] = wolfX[i];
+                        wolfY[i] = wolfY[i];
+                        break;
+                    case 6:
+                        wolfX[i] = wolfX[i] + 1;
+                        wolfY[i] = wolfY[i];
+                        break;
+                    case 7:
+                        wolfX[i] = wolfX[i] - 1;
+                        wolfY[i] = wolfY[i] - 1;
+                        break;
+                    case 8:
+                        wolfX[i] = wolfX[i];
+                        wolfY[i] = wolfY[i] - 1;
+                        break;
+                    case 9:
+                        wolfX[i] = wolfX[i] + 1;
+                        wolfY[i] = wolfY[i] - 1;
+                        break;
+                }
+
             //ограничения передвижения волка (квадрат 20 на 20)
-            if (wolfX[i] == 0)     //Если вышел за левую грань   
+            if (wolfX[i] == 0)  wolfX[i] = 1;   //Если вышел за левую грань   
+
+            if (wolfX[i] == 21) wolfX[i] = 20;  //Если вышел за правую грань
+ 
+            if (wolfY[i] == 0)  wolfY[i] = 1;  //Если вышел за нижнюю грань
+
+            if (wolfY[i] == 21) wolfY[i] = 20; //Если вышел за верхнюю грань
+ 
+
+            for (int j = 0; j <= numOfRabbits; j++)
             {
-                wolfX[i] = 1;
+                if ((rabbitX[j] == wolfX[i]) && (rabbitY[j] == wolfY[i])) isRabbitHere[4] = true;
             }
-            if (wolfX[i] == 21)   //Если вышел за правую грань
+
+            if (isRabbitHere[5])
             {
-                wolfX[i] = 20;
+                for (int j = 0; j <= numOfRabbits; j++)
+                {
+                    if ((rabbitX[j] == wolfX[i]) && (rabbitY[j] == wolfY[i]))
+                    {
+                        cellPos.x = rabbitX[j];
+                        cellPos.y = rabbitY[j];
+                        tilemap.SetTile(cellPos, groundTile);
+                        rabbitX[j] = -1;
+                        rabbitY[j] = -1;
+                        isRabbitAlive[j] = false;
+                    }
+                }
+
             }
-            if (wolfY[i] == 0)    //Если вышел за нижнюю грань
-            {
-                wolfY[i] = 1;
-            }
-            if (wolfY[i] == 21)  //Если вышел за верхнюю грань
-            {
-                wolfY[i] = 20;
-            }
+
             //                      Y ^
             //Система координат XY    | - >
             //                            X
@@ -265,9 +351,10 @@ public class setTile : MonoBehaviour
 
     void FixedUpdate()
     {
-        rabbitMove();                               //Передвижение всех кроликов
-        Time.fixedDeltaTime = Time.fixedDeltaTime * Time.timeScale; //Задержка перед следующим действием 
         wolfMove();
+        rabbitMove();   //Передвижение всех кроликов
+        Time.fixedDeltaTime = Time.fixedDeltaTime * Time.timeScale; //Задержка перед следующим действием 
+
         Debug.Log(numOfRabbits+1);
     }
 
